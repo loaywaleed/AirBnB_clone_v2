7eb5df1db0a875fa3c56aa33ea6ff163371af503
loay <loaywaleed9@gmail.com>
2023-09-19 23:04:37 +0300

    adding filter and delete to engine

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -8,8 +8,14 @@ class FileStorage:
     __file_path = 'file.json'
     __objects = {}
 
-    def all(self):
+    def all(self, cls=None):
         """Returns a dictionary of models currently in storage"""
+        if cls is not None:
+            filter = {}
+            for key, value in self.__objects.items():
+                if type(value) is cls:
+                    setattr(filter, key, value)
+            return filter
         return FileStorage.__objects
 
     def new(self, obj):
@@ -36,15 +42,23 @@ class FileStorage:
         from models.review import Review
 
         classes = {
-                    'BaseModel': BaseModel, 'User': User, 'Place': Place,
-                    'State': State, 'City': City, 'Amenity': Amenity,
-                    'Review': Review
-                  }
+            'BaseModel': BaseModel, 'User': User, 'Place': Place,
+            'State': State, 'City': City, 'Amenity': Amenity,
+            'Review': Review
+        }
         try:
             temp = {}
             with open(FileStorage.__file_path, 'r') as f:
                 temp = json.load(f)
                 for key, val in temp.items():
-                        self.all()[key] = classes[val['__class__']](**val)
+                    self.all()[key] = classes[val['__class__']](**val)
         except FileNotFoundError:
             pass
+
+    def delete(self, obj=None):
+        """method to delete objects if they exist"""
+        if obj is None:
+            return
+        obj_key = obj.__class__.__name__ + '.' + obj.id
+        if obj_key in self.__objects:
+            del self.__objects[obj_key]
