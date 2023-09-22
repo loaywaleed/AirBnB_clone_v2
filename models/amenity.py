be38a84c74a7040c63bd99a5622a6d10e184df9a
loay <loaywaleed9@gmail.com>
2023-09-22 18:52:24 +0300

    testing many to many

--- models/amenity.py
+++ models/amenity.py
@@ -1,7 +1,18 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from models import type_storage
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
+from models.place import place_amenity
 
 
-class Amenity(BaseModel):
-    name = ""
+class Amenity(BaseModel, Base):
+    __tablename__ = 'amenities'
+    if type_storage == 'db':
+        name = Column(String(128), nullable=False)
+        place_amenities = relationship(
+            'Place', secondary=place_amenity)
+
+    else:
+        name = ""
--- models/place.py
+++ models/place.py
@@ -1,16 +1,24 @@
 #!/usr/bin/python3
 """ Place Module for HBNB project """
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String, Integer, Float, ForeignKey
+from sqlalchemy import Column, String, Integer, Float, ForeignKey, Table
 from models import type_storage
 from sqlalchemy.orm import relationship
 from models.review import Review
+from models.amenity import Amenity
+
+if type_storage == 'db':
+    place_amenity = Table('place_amenity', Base.metadata,
+                          Column("place_id", String(60), ForeignKey(
+                              'places.id'), nullable=False),
+                          Column("amenity_id", String(60), ForeignKey(
+                              'amenities.id'), nullable=False))
 
 
 class Place(BaseModel, Base):
     """ A place to stay """
+    __tablename__ = "places"
     if type_storage == 'db':
-        __tablename__ = "places"
         city_id = Column(String(60), ForeignKey("cities.id"), nullable=False)
         user_id = Column(String(60), ForeignKey("users.id"), nullable=False)
         name = Column(String(128), nullable=False)
@@ -23,6 +31,10 @@ class Place(BaseModel, Base):
         longitude = Column(Float, nullable=True)
         reviews = relationship('Review', backref='place',
                                cascade="all, delete, delete-orphan")
+        amenities = relationship(
+            "Amenity", secondary="place_amenity", viewonly=False)
+        amenity_ids = []
+
     else:
         city_id = ""
         user_id = ""
@@ -45,3 +57,20 @@ class Place(BaseModel, Base):
             if self.id == review.place_id:
                 lst.append(review)
         return lst
+
+    @property
+    def amenities(self):
+        """Getter attribute for amenities"""
+        from models import storage
+        lst_of_amenities = []
+        all_amenities = storage.all(Amenity)
+        for amenity in all_amenities:
+            if self.id == amenity.id:
+                lst_of_amenities.append(amenity)
+        return lst_of_amenities
+
+    @amenities.setter
+    def amenities(self, obj):
+        """Setter attribute for amenities"""
+        if isinstance(obj, Amenity):
+            self.amenity_ids.append(obj.id)
