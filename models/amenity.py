353e6a8c8f0881e5d41882a9391493c2e8b5e356
loay <loaywaleed9@gmail.com>
2023-09-22 20:22:47 +0300

    minor change

--- models/amenity.py
+++ models/amenity.py
@@ -11,6 +11,7 @@ class Amenity(BaseModel, Base):
     __tablename__ = 'amenities'
     if type_storage == 'db':
         name = Column(String(128), nullable=False)
+        place_amenities = relationship('Place', secondary=place_amenity)
 
     else:
         name = ""
