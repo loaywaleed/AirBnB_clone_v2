7f40cad21ca0aa668423e97f7126af32f20c76c2
loay <loaywaleed9@gmail.com>
2023-09-22 15:38:38 +0300

    hot-fix

--- models/place.py
+++ models/place.py
@@ -8,6 +8,7 @@ from models import type_storage
 class Place(BaseModel, Base):
     """ A place to stay """
     if type_storage == 'db':
+        __tablename__ = "places"
         city_id = Column(String(60), ForeignKey("cities.id"), nullable=False)
         user_id = Column(String(60), ForeignKey("users.id"), nullable=False)
         name = Column(String(128), nullable=False)
