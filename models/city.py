708f543728e672f1698931b50f89636650ec6d64
loay <loaywaleed9@gmail.com>
2023-09-22 15:36:18 +0300

    updated places class

--- models/city.py
+++ models/city.py
@@ -3,6 +3,7 @@
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, ForeignKey, Constraint
 import models
+from sqlalchemy.orm import relationship
 
 
 class City(BaseModel, Base):
@@ -11,7 +12,8 @@ class City(BaseModel, Base):
     if models.type_storage == "db":
         name = Column(String(128), nullable=False)
         state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
-
+        places = relationship('Place', backref='cities',
+                              cascade='all, delete, delete-orphan')
     else:
         name = ""
         state_id = ""
--- models/user.py
+++ models/user.py
@@ -3,6 +3,8 @@
 from models.base_model import BaseModel, Base
 from models import type_storage
 from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
+from models.place import Place
 
 
 class User(BaseModel, Base):
@@ -13,6 +15,8 @@ class User(BaseModel, Base):
         password = Column(String(128), nullable=False)
         first_name = Column(String(128), nullable=True)
         last_name = Column(String(128), nullable=True)
+        places = relationship('Place', backref='user',
+                              cascade='all, delete, delete orphan')
 
     else:
         email = ''
