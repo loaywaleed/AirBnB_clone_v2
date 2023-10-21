52fbbdcf089f8d63239e41b92f3b9d716b4aa5ff
= <=>
2023-10-21 21:51:01 +0300

    added hbnb

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Module that initiates flask"""
+
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def home():
+    """returns string html"""
+    return 'Hello HBNB!'
+
+
+def hbnb():
+    """returns hbnb"""
+    return 'HBNB'
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port='5000')
