326c7e872bf79cabc98f1c0e5588994d48d8f661
= <=>
2023-10-21 22:38:03 +0300

    templates

---
+++ web_flask/5-number_template.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3
+"""Module that initiates flask"""
+
+from flask import Flask, render_template
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def home():
+    """returns string html"""
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """returns hbnb"""
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def cisfun(text):
+    """returns c and name of route"""
+    return 'C ' + text.replace('_', ' ')
+
+
+@app.route('/python', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def pythoniscool(text="is cool"):
+    """returns python and name of route"""
+    return 'Python ' + text.replace('_', ' ')
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def isanumber(n):
+    """returns python and name of route"""
+    return "{:d} is a number".format(n)
+
+
+@app.route('/number_template/<int:n>')
+def number_page(n):
+    """returning an html template"""
+    return render_template('5-number.html', number=n)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port='5000')
