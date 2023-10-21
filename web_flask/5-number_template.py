cfe6f932e09cec118fc23f968d733082a5dae4c7
= <=>
2023-10-21 22:42:32 +0300

    strict slashes fix

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -36,10 +36,10 @@ def isanumber(n):
     return "{:d} is a number".format(n)
 
 
-@app.route('/number_template/<int:n>')
+@app.route('/number_template/<int:n>', strict_slashes=False)
 def number_page(n):
     """returning an html template"""
-    return render_template('5-number.html', number=n)
+    return render_template('5-number.html', n=n)
 
 
 if __name__ == '__main__':
